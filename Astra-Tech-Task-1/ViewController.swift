d9ced40189de188a2ff6961f65f34be7783d41fb
AhmedEllashy <ahmadellashy2000@gmail.com>
2024-10-15 19:23:19 +0300

    Initial Commit

---
+++ Astra-Tech-Task-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Astra-Tech-Task-1
+//
+//  Created by Ahmad Ellashy on 15/10/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
